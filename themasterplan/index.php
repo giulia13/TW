408c1d16d9642b91a10f3df777634f7310da04c4
giulia13 <ggabriela_12@hotmail.com>
2013-09-24 19:02:40 -0300

    Revert "dfd"
    This reverts commit 3b8d067077a5494c4e7eb63b1cda54cddbc69860.

--- themasterplan/index.php
+++ themasterplan/index.php
@@ -10,13 +10,12 @@
 	<div>
 			<div id= "post">
 					<div id = "titulopost"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></div>
-					<div id = "data"><?php the_time('d/M/Y') ?></div>
+					<div id = "data"><p><?php the_time('F jS, Y') ?></p></div>
 				
 				<div class = "texto"><p><?php the_content(); ?></p></div>
 					<div id ="caixa1"
-						<p>Postado por: <?php the_author_link(); ?> </p>
+						<p> Postado por: <?php the_author_link(); ?> </p>
 						<p><?php the_tags(); ?></p>
-						<p><?php edit_post_link('(editar post)'); ?></p>
 					</div>
 					<div id = "caixa2">
 						<p id = "comentarios"><?php comments_popup_link('deixe um comentário', 'alguém falou ', '% comentários'); ?>
